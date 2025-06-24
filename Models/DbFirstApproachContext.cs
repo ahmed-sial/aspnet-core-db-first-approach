481ae8fe617eacb6863984a17cd9c923ababe297
Ahmed Hassan <ahmed.hassan398@outlook.com>
2025-06-24 20:59:39 +0500

    Add project files.

---
+++ Models/DbFirstApproachContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirstApproach.Models;
+
+public partial class DbFirstApproachContext : DbContext
+{
+    public DbFirstApproachContext()
+    {
+    }
+
+    public DbFirstApproachContext(DbContextOptions<DbFirstApproachContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3213E83F59DECB96");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Cgpa)
+                .HasColumnType("decimal(3, 2)")
+                .HasColumnName("cgpa");
+            entity.Property(e => e.FName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("f_name");
+            entity.Property(e => e.LName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("l_name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataBaseFirstApproach.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    [Display(Name = "First Name")]
+    public string FName { get; set; } = null!;
+
+    [Display(Name = "Last Name")]
+    public string LName { get; set; } = null!;
+
+    [Range(0.0, 4.0)]
+    [Display(Name = "CGPA")]
+    public decimal Cgpa { get; set; }
+}
